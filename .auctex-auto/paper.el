c334963d508f240cc4b354869f910624908e94ba
muke <muke101@protonmail.com>
2026-03-15 21:19:38 +0000

    initial commit

---
+++ .auctex-auto/paper.el
@@ -0,0 +1,30 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "paper"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("elsarticle" "") ("article" "") ("acmart" "sigconf" "screen" "review")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("hyperref" "hidelinks") ("graphicx" "") ("listings" "") ("color" "") ("caption" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "ead")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "acmart"
+    "acmart10")
+   (TeX-add-symbols
+    "BibTeX")
+   (LaTeX-add-bibliographies
+    "sample-base"))
+ :latex)
+
